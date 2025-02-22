dbbcbe1c734728775e7289c372a12f5bcc4e20ba
JanMajer86 <majer.jan.1986@gmail.com>
2025-02-22 13:29:30 +0100

    login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,38 +1,48 @@
-import { Form } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Login";
 import FormRow from "../components/FormRow";
+import customFetch from "../utils/customFetch";
 
 export const action = async ({ request }) => {
 	const formData = await request.formData();
 	const data = Object.fromEntries(formData);
-	console.log(data);
-	return null;
+	try {
+		await customFetch.post("/auth/login", data);
+		return redirect("/all-bochaneks");
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
 };
 
 const Login = () => {
+	const isSubmitting = useNavigation().state === "submitting";
+
 	return (
 		<Wrapper>
 			<Form method="POST">
 				<div className="form-row">
 					<label htmlFor="user">select user</label>
 
-					<input type="radio" name="user" id="honza" value="honza" required />
-					<label htmlFor="honza" className="btn">
-						honza
+					<input type="radio" name="user" id="Honza" value="Honza" required />
+					<label htmlFor="Honza" className="btn">
+						Honza
 					</label>
 					<input
 						type="radio"
 						name="user"
-						id="kristyna"
-						value="kristýna"
+						id="Kristýna"
+						value="Kristýna"
 						required
 					/>
-					<label htmlFor="kristyna" className="btn">
-						kristýna
+					<label htmlFor="Kristýna" className="btn">
+						Kristýna
 					</label>
 				</div>
-				<FormRow type="text" name="password" />
-				<button type="submit">login</button>
+				<FormRow type="password" name="password" />
+				<button type="submit">
+					{isSubmitting ? "logging in..." : "login"}
+				</button>
 			</Form>
 		</Wrapper>
 	);
